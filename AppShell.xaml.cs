3f3616704c92c5bbbc7c197a6fc2f85ea77c5b44
Enes481 <enest5529@gmail.com>
2024-11-25 21:02:54 +0300

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorProject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Calculate.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace CalculatorProject
+{
+    public static class Calculate
+    {
+        public static double DoCalculation(double val1, double val2, string operatorMath)
+        {
+            double result = 0;
+
+            switch (operatorMath)
+            {
+                case "/":
+                    result = val1 / val2;
+                    break;
+                case "-":
+                    result = val1 - val2;
+                    break;
+                case "*":
+                    result = val1 * val2;
+                    break;
+                case "+":
+                    result = val1 + val2;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
+
+
